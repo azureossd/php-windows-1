d39edba3ae0805e3ef8bc7b9a7a06d313dd07023
Christopher Maldonado <chmald@microsoft.com>
2021-01-13 15:09:25 -0600

    fixed elseif statement

--- part2.php
+++ part2.php
@@ -17,7 +17,7 @@ foreach($modules as $module) {
             echo "LDAP extension has been installed, you are ready to go! </br></br>";
         }
     } else {
-        if($module == "ldap") {
+        if($module == "mongodb") {
             echo "PHP Error: Class 'MongoDB\Client' not found </br></br>";
         } else if ($module == "mongodb") {
             echo "Mongodb extension has been installed, find it from installed extensions, you are ready to go! </br></br>";
