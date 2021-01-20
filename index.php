1c408697690b1ecdc255ce85a830c0211eb32dd1
Christopher Maldonado <chmald@microsoft.com>
2021-01-20 12:15:12 -0600

    name change

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * Azure App Service L200 Lab
+ * Azure App Service Specialist Lab
  * Open Source Technologies - PHP Windows
  */
 
- echo "Welcome to the PHP L200 Windows Lab 1. You can use the following links to access each part of this lab, or access them via <site-name>/part#.php" . "</br></br>";
+ echo "Welcome to the PHP Specialist Windows Lab 1. You can use the following links to access each part of this lab, or access them via <site-name>/part#.php" . "</br></br>";
 
  echo "<a href='/part1.php'>Part 1: Modify PHP Settings</a></br>";
  echo "<a href='/part2.php'>Part 2: Modify PHP Extension</a></br>";
